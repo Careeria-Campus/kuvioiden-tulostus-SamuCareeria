72fd3b15e2c84f65d96f46666f902c593af7f783
Samuli Edelman <samu09.makitalo@gmail.com>
2025-12-15 09:28:40 +0200

    efwf

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,49 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-           
+            Console.Write("Kuinka ison neliön haluat tehdä: ");
+            int koko = int.Parse(Console.ReadLine());
+
+            TulostaNelio(koko);
         }
 
         public static void TulostaNelio(int sivunPituus)
         {
-           
+            // Ylärivi
+            for (int i = 0; i < sivunPituus; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            // Keskirivit
+            for (int i = 0; i < sivunPituus - 2; i++)
+            {
+                for (int e = 0; e < sivunPituus; e++)
+                {
+                    if (e == 0 || e == sivunPituus - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // Alarivi
+            for (int i = 0; i < sivunPituus; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
         }
     }
 }
